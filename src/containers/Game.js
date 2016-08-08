285faa8db792e39c077940c09e0432af9c7a3924
Zachary Maybury <maybury@iweave.com>
2016-08-08 01:59:03 -0400

    Adding github icon

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -18,9 +18,12 @@ export default class Game extends Component {
 
 	render() {
 		return (
-			<div className="ui basic text container segment" style={{display:"flex",flex:1,flexDirection:"column"}}>
+			<div className="ui basic text container segment" style={{display:"flex",flex:1,flexDirection:"column",backgroundColor:"white"}}>
 				<h1 className="ui centered header">React Checkers</h1>
 				<Board/>
+				<h2 className="ui right aligned header">
+					<i className="github link icon" href="https://github.com/zmaybury/react-checkers"/>
+				</h2>
 			</div>
 		);
 	}
